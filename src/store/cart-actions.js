5304c3d0264c308e03ef733e7f3a5f7f62d9a21a
piskova <v.piskova@beetroot.se>
2025-02-19 17:00:53 +0200

    Fix price and initial load bugs

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -25,7 +25,10 @@ export const fetchCartData = () => {
 
     try {
       const cartData = await fetchData();
-      cartActions.replaceCart(cartData);
+      cartActions.replaceCart({
+        items: cartData.items || [],
+        totalQuantity: cartData.totalQuantity,
+      });
       dispatch(
         uiActions.showNotification({
           title: "Updated",
@@ -47,8 +50,6 @@ export const fetchCartData = () => {
 
 export const sendCartData = (cart) => {
   return async (dispatch) => {
-    console.log("cart");
-
     dispatch(
       uiActions.showNotification({
         title: "Sending...",
@@ -60,7 +61,13 @@ export const sendCartData = (cart) => {
     const sendRequest = async () => {
       const response = await fetch(
         "https://react-redux-cart-bbc11-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
-        { method: "PUT", body: JSON.stringify(cart) }
+        {
+          method: "PUT",
+          body: JSON.stringify({
+            items: cart.items,
+            totalQuantity: cart.totalQuantity,
+          }),
+        }
       );
 
       if (!response.ok) {
